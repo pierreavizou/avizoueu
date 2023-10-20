43ae9a50d72ff8d67abe07b571566739ffe3f432
Pierre A <pierre@avizou.eu>
2023-10-20 17:01:03 +0200

    return image directly - log more data

--- app/img/[filename]/route.tsx
+++ app/img/[filename]/route.tsx
@@ -17,18 +17,13 @@ export async function GET(
   { params }: { params: { filename: string } },
 ) {
   const headersList = headers();
-  // console.log("headersList", headersList);
-  console.log("req.ip", req.ip);
-  console.log("req.geo", req.geo);
+
   let ip = req.ip ?? headersList.get("x-real-ip");
   const forwardedFor = headersList.get("x-forwarded-for");
   if (!ip && forwardedFor) {
     ip = forwardedFor.split(",").at(0) ?? "Unknown";
   }
-  console.log("ip", ip);
-
   const userAgent = headersList.get("user-agent");
-  console.log("userAgent", userAgent);
   const geo = {
     country: headersList.get("x-vercel-ip-country"),
     countryRegion: headersList.get("x-vercel-ip-country-region"),
@@ -36,35 +31,91 @@ export async function GET(
     longitude: headersList.get("x-vercel-ip-longitude"),
     timezone: headersList.get("x-vercel-ip-timezone"),
   };
-  console.log("geo", geo);
-
   const acceptLanguage = headersList.get("accept-language");
-  console.log("acceptLanguage", acceptLanguage);
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        <img
-          src={`https://wekujktwjytshhlqfrew.supabase.co/storage/v1/object/public/Assets/${params.filename}`}
-          alt=""
-          width={401}
-          height={123}
-        />
-      </div>
-    ),
-    {
-      // width: 1200,
-      // height: 630,
-      width: 401,
-      height: 123,
-      emoji: "twemoji",
+  let metadata;
+  if (req.nextUrl.searchParams.has("metadata")) {
+    const decoded = Buffer.from(
+      req.nextUrl.searchParams.get("metadata")!,
+      "base64",
+    ).toString("utf8");
+    req.nextUrl.searchParams.delete("metadata");
+    try {
+      metadata = JSON.parse(decoded);
+    } catch (e) {
+      console.error("Could not parse metadata to JSON", e);
+    }
+  }
+
+  const queryString = decodeURIComponent(req.nextUrl.searchParams.toString());
+  const parsedQueryString = Object.fromEntries(req.nextUrl.searchParams);
+
+  const filename = params.filename;
+
+  try {
+    const { error } = await supabase.from("access_logs").insert({
+      ip,
+      userAgent,
+      acceptLanguage,
+      geo,
+      filename,
+      queryString,
+      parsedQueryString,
+      metadata,
+    });
+    if (error) console.error("Could not save data to Supabase", error);
+  } catch (e) {
+    console.error("Could not communicated with Supabase", e);
+  }
+
+  try {
+    const img = await fetch(
+      `https://wekujktwjytshhlqfrew.supabase.co/storage/v1/object/public/Assets/${filename}`,
+    );
+
+    if (img.status !== 200) {
+      return new Response("Not found", {
+        status: 404,
+      });
+    }
+
+    return new Response(await img.arrayBuffer(), {
       headers: {
-        "cache-control": "no-cache",
+        "Content-Type": img.headers.get("Content-Type") ?? "",
+        "Cache-Control": "no-cache",
       },
-    },
-  );
+    });
+  } catch (e) {
+    console.error(e);
+    return new Response("An error occured", {
+      status: 500,
+    });
+  }
+
+  // return new ImageResponse(
+  //   (
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //       }}
+  //     >
+  //       <img
+  //         src={`https://wekujktwjytshhlqfrew.supabase.co/storage/v1/object/public/Assets/${filename}`}
+  //         alt=""
+  //         width={401}
+  //         height={123}
+  //       />
+  //     </div>
+  //   ),
+  //   {
+  //     // width: 1200,
+  //     // height: 630,
+  //     width: 401,
+  //     height: 123,
+  //     emoji: "twemoji",
+  //     headers: {
+  //       "cache-control": "no-cache",
+  //     },
+  //   },
+  // );
 }
