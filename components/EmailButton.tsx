b3282cdae7ea8033c6e0c9eadb1d09046ff079e4
Pierre A <pierre@avizou.eu>
2024-11-19 13:31:18 +0000

    change email address

--- components/EmailButton.tsx
+++ components/EmailButton.tsx
@@ -3,10 +3,10 @@ import TooltipClipboard from "./ui/tooltip-clipboard";
 export default function EmailButton() {
   return (
     <TooltipClipboard
-      text="contact@avizou.eu"
+      text="hi@avizou.eu"
       className="mt-14 inline-block rounded bg-blue-700 p-3 text-lg transition-transform selection:bg-blue-300 hover:bg-blue active:scale-90"
     >
-      contact<span className="hidden">&nbsp;</span>@
+      hi<span className="hidden">&nbsp;</span>@
       <span className="hidden">&nbsp;</span>avizou
       <span className="hidden">&nbsp;</span>.
       <span className="hidden">&nbsp;</span>eu
